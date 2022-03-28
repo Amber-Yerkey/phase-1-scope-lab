2758ad220c78a0bb4abe183f6ecf8883c263b960
Amber Frantz <amber.k.frantz@mgail.com>
2022-03-27 20:07:13 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+    return customerName
+}
+
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+    return bestCustomer
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'amber'
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'scott'
+}
+
+
+
+/*
+  
+    describe('changeLeastFavoriteCustomer()', function() {
+      it('unsuccessfully tries to reassign the least favorite customer', function() {
+        expect(changeLeastFavoriteCustomer).to.throw('Assignment to constant variable.');
+      });
+    });
+  });
+*/
